7b1ac6a550c0c4aaac39d729cf75bba9bdd3395e
Phil Marshall <dr.phil.marshall@gmail.com>
2014-10-07 23:46:43 -0700

    Nice plot of kappa map

--- evillens/gravitationalLens.py
+++ evillens/gravitationalLens.py
@@ -9,8 +9,9 @@ Created on Mon Sep 29 22:53:20 2014
 from astropy import units, constants
 from math import pi
 from astropy.cosmology import FlatLambdaCDM
-from astropy.table import Table
+from astropy.io import fits
 import numpy as np
+import matplotlib.pyplot as plt
 
 # ======================================================================
 
@@ -18,20 +19,17 @@ class GravitationalLens(object):
     '''
     An object class describing a gravitational lens system.
     '''
-    def __init__(self, Zd, Zs, Npixels = 5, xLength = .01, yLength = .01):
+    def __init__(self, Zd, Zs):
         
         self.Zd = Zd
         self.Zs = Zs
-        self.Npixels = Npixels
-        self.xLength = xLength
-        self.yLength = yLength
-        self.Dd = None
-        self.Ds = None
-        self.Dds = None
-        self.SigmaCrit = None
 
+        # Calculate distances and the critical density:
         self.cosmological = FlatLambdaCDM(H0=70, Om0=0.3)
         self.compute_distances()
+        
+        # Make a default pixel grid:
+        self.setup_grid(NX=100,NY=100,pixscale=0.1)
         
         return
 
@@ -52,29 +50,152 @@ class GravitationalLens(object):
         return
  
 # ----------------------------------------------------------------------
+
+    def setup_grid(self,NX=None,NY=None,pixscale=None):
+        '''
+        Make two arrays, x and y, that define the extent of the maps
+        - pixscale is the size of a pixel, in arcsec.
+        '''
+        if NX is not None: 
+            self.NX = NX
+        if NY is not None: 
+            self.NY = NY
+        if pixscale is not None: 
+            self.pixscale = pixscale
+               
+        xgrid = np.arange(-self.NX/2.0,self.NX/2.0,1.0)*self.pixscale
+        ygrid = np.arange(-self.NY/2.0,self.NY/2.0,1.0)*self.pixscale
+        self.x, self.y = np.meshgrid(xgrid,ygrid)
+        
+        return
+        
+# ----------------------------------------------------------------------
  
     def build_kappa_map(self):
-        raise Exception("Can't build a kappa map.\n")
+        self.kappa = None
         return
         
 # ----------------------------------------------------------------------
        
     def read_kappa_from(self,fitsfile):
-        raise Exception("Can't read in kappa maps yet.\n") 
-        #self.kappa = Table.read(fitsfile)
-        return
+        '''
+        Read a convergence map from a FITS format file, and adopt its
+        pixel grid. This means we have to extract the pixel scale from 
+        the FITS header.
+        '''
+        if fitsfile is None:
+            raise Exception("No kappa map FITS image provided.\n") 
         
+        # Open the file and read the image data:
+        hdulist = fits.open(fitsfile)
+        self.hdr = hdulist[0].header
+        self.kappa = hdulist[0].data
+        hdulist.close()
+        
+        # Extract the pixel grid information:
+        assert len(self.kappa.shape) == 2
+        assert self.kappa.shape == (self.hdr['NAXIS1'],self.hdr['NAXIS2'])
+        self.NX,self.NY = self.kappa.shape
+        self.set_pixscale()
+        
+        # Set up a new pixel grid to go with this new kappa map:
+        self.setup_grid()
+        
+        return
+
+# ----------------------------------------------------------------------
+
+    def set_pixscale(self):
+
+        # Modern FITS files:
+        if 'CD1_1' in self.hdr.keys():            
+            determinant = self.hdr['CD1_1']*self.hdr['CD2_2'] \
+                          - self.hdr['CD1_2']*self.hdr['CD2_1']
+            self.pixscale = 3600.0*np.sqrt(np.abs(determinant))
+
+        # Older FITS files:
+        elif 'CDELT1' in self.hdr.keys():
+            self.pixscale = 3600.0*np.sqrt(np.abs(self.hdr['CDELT1']*self.hdr['CDELT2']))
+
+        # Simple FITS files with no WCS information (bad):
+        else:
+            self.pixscale = 1.0
+            
+        return
+
 # ----------------------------------------------------------------------
     
     def deflect(self):
-        raise Exception("Can't compute deflections yet.\n")  
+        
+        if self.kappa is None:
+            self.alpha_x = None
+            self.alpha_y = None  
+        else:
+            raise Exception("Can't do integral yet.\n")  
+        
         return
     
 # ----------------------------------------------------------------------    
     
-    def realize(self, Nx, Ny, pixelscale):    
-        raise Exception("Can't plot anything yet.\n")  
+    def plot(self,mapname):    
+        '''
+        Plot the given map as a nice colorscale image, with contours.
+        '''
+        # Which map do we want to plot?
+        # And what options does that mean we need?
+        if mapname == "kappa":
+            img = self.kappa
+            levels = np.arange(0.1,1.5,0.2)
+            options = dict(interpolation='nearest',\
+                           origin='lower',\
+                           vmin=-0.2, \
+                           vmax=1.5)        
+        else:
+             raise ValueError("unrecognized map name %s" % mapname)
+        
+        # Generic figure setup:
+        px,py = 1,1
+        figprops = dict(figsize=(5*px,5*py), dpi=128)
+        adjustprops = dict(left=0.1,\
+                           bottom=0.1,\
+                           right=0.95,\
+                           top=0.95,\
+                           wspace=0.1,\
+                           hspace=0.1)
+        
+        # Finish setting up the options.
+        # 1) Images need extents, if x and y are not pixel numbers:
+        options['extent'] = (np.min(self.x),np.max(self.x),\
+                             np.min(self.y),np.max(self.y))
+        # 2) The cubehelix map is linear grayscale on a BW printer
+        options['cmap'] = plt.get_cmap('cubehelix')
+        
+        # Start the figure:
+        fig = plt.figure(**figprops)
+        fig.subplots_adjust(**adjustprops)
+        plt.clf()
+
+        # Plot a colored pixel map and overlay some contours:
+        plt.imshow(img, **options)
+        plt.contour(self.x, self.y, img, levels,colors=('k',))
+        
+        # Annotate the plot:
+        plt.xlabel('x / arcsec')
+        plt.ylabel('y / arcsec')
+        plt.axes().set_aspect('equal')
+
+        # If we're in a notebook, display the plot. 
+        # Otherwise, make a PNG.
+        try:
+            __IPYTHON__
+            plt.show()
+        except NameError:
+            pngfile = mapname+'.png'
+            plt.savefig(pngfile)
+            print "Saved plot to "+pngfile
+            
         return
+
 
 # ----------------------------------------------------------------------
 
@@ -99,5 +220,7 @@ if __name__ == '__main__':
     print "  cf. Dds = ", lens.Dds
     print "Critical density = ",lens.SigmaCrit
 
-        
+    lens.read_kappa_from("examples/test_kappa.fits")
+    lens.plot("kappa")
+            
 # ======================================================================
