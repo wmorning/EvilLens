Changed write kappa method so that it includes pixscale in header.  Fixes #18 .  Also changed units in build kappa map so that it follows Saas Fe, and analytic solution so that it has the same consistency.

evillens/analyticSIELens.py
@@ -32,12 +32,11 @@ class AnalyticSIELens(evil.GravitationalLens):
     def build_kappa_map(self):
          
          # First compute Einstein radius, in arcsec:
-         Sigma_ellipsoid = self.sigma**2/(2.0*constants.G*self.Dd)  # Missing some things here...        
-         thetaE = units.Quantity.to(Sigma_ellipsoid,units.solMass/units.Mpc**2) / self.SigmaCrit 
-         
+         b = 4*np.pi *(self.sigma/constants.c)**2*(np.sqrt(1-self.q**2)/np.arcsin(np.sqrt(1-self.q**2)))*self.Dds/self.Ds
+         self.b = b.decompose()
          
          # Now compute kappa = thetaE/(2*theta) where theta = elliptical radial position
-         self.kappa = thetaE.value/(2.0*np.sqrt(self.q*(self.x-self.centroid[0])**2+(self.y-self.centroid[1])**2))
+         self.kappa = self.b.value /(2.0*np.sqrt(self.q*(self.x-self.centroid[0])**2+(self.y-self.centroid[1])**2))
 
          return
          
@@ -46,12 +45,14 @@ class AnalyticSIELens(evil.GravitationalLens):
     def deflect(self):
         
         # First convert image coordinates to angle from semimajor axis.
-        image_theta = np.arctan2(self.image_y-self.centroid[1],self.image_x-self.centroid[0]) # check...
+        #image_theta = np.arctan2(self.image_y-self.centroid[1],self.image_x-self.centroid[0]) # check...
 
         # Deflect analytically
-        self.alpha_x = (self.q/np.sqrt(1-self.q**2))*np.arctan(np.sqrt((1-self.q**2)/(self.q**2*np.cos(image_theta)**2+np.sin(image_theta)**2))*np.cos(image_theta))
-        self.alpha_y = (self.q/np.sqrt(1-self.q**2))*np.arctanh(np.sqrt((1-self.q**2)/(self.q**2*np.cos(image_theta)**2+np.sin(image_theta)**2))*np.sin(image_theta))
-
+        #self.alpha_x = (self.b.value/np.sqrt(1-self.q**2))*np.arctan(np.sqrt((1-self.q**2)/(self.q**2*np.cos(image_theta)**2+np.sin(image_theta)**2))*np.cos(image_theta))
+        #self.alpha_y = (self.b.value/np.sqrt(1-self.q**2))*np.arctanh(np.sqrt((1-self.q**2)/(self.q**2*np.cos(image_theta)**2+np.sin(image_theta)**2))*np.sin(image_theta))
+        
+        self.alpha_x = (self.b.value/np.sqrt(1-self.q**2)) * np.arctan((self.image_x-self.centroid[0])*np.sqrt(1-self.q**2)/np.sqrt(self.q**2*(self.image_x-self.centroid[0])**2+(self.image_y-self.centroid[1])**2))        
+        self.alpha_y = (self.b.value/np.sqrt(1-self.q**2)) * np.arctan((self.image_y-self.centroid[1])*np.sqrt(1-self.q**2)/np.sqrt(self.q**2*(self.image_x-self.centroid[0])**2+(self.image_y-self.centroid[1])**2))
         return
 
 # ======================================================================

evillens/gravitationalLens.py
@@ -69,14 +69,14 @@ class GravitationalLens(object):
         if offset is not None:
             self.offset = offset
             
-        xgrid = np.arange(-self.NX/2.0,self.NX/2.0,1.0)*self.pixscale
-        ygrid = np.arange(-self.NY/2.0,self.NY/2.0,1.0)*self.pixscale
+        xgrid = np.arange(-self.NX/2.0,(self.NX)/2.0,1.0)*self.pixscale+self.pixscale
+        ygrid = np.arange(-self.NY/2.0,(self.NY)/2.0,1.0)*self.pixscale+self.pixscale
         
         #WRM:  here we build new grid for the image and source pixels,
         #      purposefully misaligned with the kappa pixels, so no NaNs occur.        
         self.pixel_offset = self.offset*self.pixscale/self.n
-        image_xgrid = np.arange(-self.NX/2.0,self.NX/2.0,1.0/self.n)*self.pixscale-self.pixel_offset
-        image_ygrid = np.arange(-self.NY/2.0,self.NY/2.0,1.0/self.n)*self.pixscale-self.pixel_offset
+        image_xgrid = np.arange(-self.NX/2.0,(self.NX)/2.0,1.0/self.n)*self.pixscale-self.pixel_offset
+        image_ygrid = np.arange(-self.NY/2.0,(self.NY)/2.0,1.0/self.n)*self.pixscale-self.pixel_offset
         
         self.x, self.y = np.meshgrid(xgrid,ygrid)
         self.image_x, self.image_y = np.meshgrid(image_xgrid,image_ygrid)
@@ -252,6 +252,10 @@ class GravitationalLens(object):
     def write_kappa_to( self, fitsfile="kappa_map.fits"):
         
         hdu = fits.PrimaryHDU(self.kappa)
+        
+        hdu.header['CDELT1'] = self.pixscale / 3600.0
+        hdu.header['CDELT2'] = self.pixscale / 3600.0
+        
         hdu.writeto(fitsfile)
        
         return