f9bc50320cd94a55ea21725da3c31f4ee5212c17
Warren Morningstar <warrenmorningstar@dn525it4.sunet>
2014-11-03 17:08:43 -0800

    added python script to turn RBG JPG image into black and white FITS image.  WARNING:  Does not write NAXIS1 and NAXIS2 correctly.

---
+++ evillens/JPGconverter.py
@@ -0,0 +1,42 @@
+"""
+Image converter script for Jpg files to make fits
+"""
+
+
+import Image
+
+from astropy.io import fits as fits
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def JPG_to_FITS(jpgimage):
+
+    image = Image.open(jpgimage)
+    image.getcolors()
+    r,g,b = image.split()
+    
+    xsize,ysize = image.size  
+    
+    rdata = r.getdata()    
+    gdata = g.getdata()
+    bdata = b.getdata()
+    
+    npr = np.reshape(rdata, (ysize, xsize))
+    npg = np.reshape(gdata, (ysize, xsize))
+    npb = np.reshape(bdata, (ysize, xsize))
+    
+    magnitude = np.sqrt(npr**2+npg**2+npb**2)
+    
+    hdu = fits.PrimaryHDU(data=magnitude)
+    hdu.header['CDELT1'] = 2.0/xsize/3600.0
+    hdu.header['CDELT2'] = 1.0/xsize/3600.0
+    hdu.header['NAXIS1'],hdu.header['NAXIS2'] = magnitude.shape
+    print(hdu.header)
+    hdu.writeto('output.fits', clobber=True)
+    
+    print(hdu.header)
+    print(hdu.filename())
+    
+    return 
+    
