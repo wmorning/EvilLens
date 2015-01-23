14a92096676e7f5a356c2cc33243a01e7590956d
Warren Morningstar <wmorning@DN525jai.SUNet>
2015-01-23 14:21:34 -0800

    SIE can now be rotated in all 3 dimensions

--- evillens/analyticSIELens.py
+++ evillens/analyticSIELens.py
@@ -22,36 +22,58 @@ class AnalyticSIELens(evil.GravitationalLens):
         super(AnalyticSIELens, self).__init__(*args, **kwargs)
         self.sigma = 200.0 * units.km/units.s
         self.q = 0.75
+        self.q3 = self.q
         self.centroid = [0.01,0.01]  #WRM: centroid offsets center of map from
         self.r_c = 0.0               #     origin to avoid divergence at a pixel
-        
+        self.inclination=0.0
+        self.rotation = 0.0
         return    
         
 # ----------------------------------------------------------------------
  
-    def build_kappa_map(self, sigma=None,q=None,centroid=None, r_c = None):
+    def build_kappa_map(self, sigma=None,q3=None,centroid=None, r_c = None, inclination = None,rotation=0):
         '''
         Create kappa map using current parameters.  Also allows 
         user to manually set lens parameters if they want to.
+        - sigma is central stellar velocity dispersion in km/s
+        - q3 is axis ratio of oblate ellipsoid
+        - centroid is x,y coordinates of center
+        - r_c is core radius
+        - inclination is the inclination relative to the lens plane
+        - rotation is the angle between the semi-major axis and the x-axis 
         '''
         if sigma is not None:
             self.sigma = sigma * units.km/units.s
-        if q is not None:
-            self.q = q
+        if q3 is not None:
+            self.q3 = q3
         if centroid is not None:
             assert len(centroid)==2
             self.centroid = centroid
         if r_c is not None:
             self.r_c = r_c
+        if inclination is not None:
+            self.inclination = inclination
+        if rotation is not None:
+            self.rotation = rotation
+        
+        #rotate axes by the rotation angle around the centroid of the lens
+        xprime = np.cos(self.rotation)*(self.x-self.centroid[0])+np.sin(self.rotation)*(self.y-self.centroid[1])
+        yprime = -np.sin(self.rotation)*(self.x-self.centroid[0])+np.cos(self.rotation)*(self.y-self.centroid[1])
+        
+           
+        #get 2-d axis ratio from 3d axis ratio and inclination
+        self.q = np.sqrt((self.q3*np.cos(self.inclination))**2+np.sin(self.inclination)**2)
          
-         
-        # First compute Einstein radius, in arcsec:
-        b = 4*np.pi *(self.sigma/constants.c)**2*(np.sqrt(1-self.q**2)/np.arcsin(np.sqrt(1-self.q**2)))*self.Dds/self.Ds
+        #compute Einstein radius, in arcsec:
+        b = 4*np.pi *(self.sigma/constants.c)**2*(np.sqrt(1-self.q3**2)/np.arcsin(np.sqrt(1-self.q3**2)))*self.Dds/self.Ds
         self.b = b.decompose()*(3600.0*180.0/np.pi)
          
          # Now compute kappa using lens equation from Saas Fe.
-        self.kappa = self.b.value /(2.0*np.sqrt(self.q**2*((self.x-self.centroid[0])**2+self.r_c**2)+(self.y-self.centroid[1])**2))
-
+        #self.kappa = self.b.value /(2.0*np.sqrt(self.q**2*((self.x-self.centroid[0])**2+self.r_c**2)+(self.y-self.centroid[1])**2))
+        
+        #compute kappa in rotated frame.        
+        self.kappa = self.b.value /(2.0*np.sqrt(self.q**2*((xprime)**2+self.r_c**2)+(yprime)**2))
+        
         return
          
 # ----------------------------------------------------------------------
@@ -60,14 +82,26 @@ class AnalyticSIELens(evil.GravitationalLens):
         
         # First convert image coordinates to angle from semimajor axis.
         #image_theta = np.arctan2(self.image_y-self.centroid[1],self.image_x-self.centroid[0]) # check...
-
+        
+        #rotate axes by the rotation angle around the centroid of the lens
+        xprime = np.cos(self.rotation)*(self.image_x-self.centroid[0])+np.sin(self.rotation)*(self.image_y-self.centroid[1])
+        yprime = -np.sin(self.rotation)*(self.image_x-self.centroid[0])+np.cos(self.rotation)*(self.image_y-self.centroid[1])
+        
+        
         # Deflect analytically.  This is old way, but it should be the same.
         #self.alpha_x = (self.b.value/np.sqrt(1-self.q**2))*np.arctan(np.sqrt((1-self.q**2)/(self.q**2*np.cos(image_theta)**2+np.sin(image_theta)**2))*np.cos(image_theta))
         #self.alpha_y = (self.b.value/np.sqrt(1-self.q**2))*np.arctanh(np.sqrt((1-self.q**2)/(self.q**2*np.cos(image_theta)**2+np.sin(image_theta)**2))*np.sin(image_theta))
         
         # Deflect analytically using equation from Saas Fe
-        self.alpha_x = (self.b.value/np.sqrt(1-self.q**2)) * np.arctan((self.image_x-self.centroid[0])*np.sqrt(1-self.q**2)/(np.sqrt(self.q**2*((self.image_x-self.centroid[0])**2+self.r_c**2)+(self.image_y-self.centroid[1])**2)+self.r_c))        
-        self.alpha_y = (self.b.value/np.sqrt(1-self.q**2)) * np.arctanh((self.image_y-self.centroid[1])*np.sqrt(1-self.q**2)/(np.sqrt(self.q**2*((self.image_x-self.centroid[0])**2+self.r_c**2)+(self.image_y-self.centroid[1])**2)+self.q**2*self.r_c))
+        #self.alpha_x = (self.b.value/np.sqrt(1-self.q**2)) * np.arctan((self.image_x-self.centroid[0])*np.sqrt(1-self.q**2)/(np.sqrt(self.q**2*((self.image_x-self.centroid[0])**2+self.r_c**2)+(self.image_y-self.centroid[1])**2)+self.r_c))        
+        #self.alpha_y = (self.b.value/np.sqrt(1-self.q**2)) * np.arctanh((self.image_y-self.centroid[1])*np.sqrt(1-self.q**2)/(np.sqrt(self.q**2*((self.image_x-self.centroid[0])**2+self.r_c**2)+(self.image_y-self.centroid[1])**2)+self.q**2*self.r_c))
+        
+        #Deflect analytically for rotated SIE.  Then rotate back to original coordinate bases.
+        alpha_x_prime = (self.b.value/np.sqrt(1-self.q**2)) * np.arctan((xprime)*np.sqrt(1-self.q**2)/(np.sqrt(self.q**2*((xprime)**2+self.r_c**2)+(yprime)**2)+self.r_c))        
+        alpha_y_prime = (self.b.value/np.sqrt(1-self.q**2)) * np.arctanh((yprime)*np.sqrt(1-self.q**2)/(np.sqrt(self.q**2*((xprime)**2+self.r_c**2)+(yprime)**2)+self.q**2*self.r_c))
+        self.alpha_x = np.cos(self.rotation)*alpha_x_prime-np.sin(self.rotation)*alpha_y_prime
+        self.alpha_y = np.sin(self.rotation)*alpha_x_prime+np.cos(self.rotation)*alpha_y_prime
+               
         return
 
 # ======================================================================
