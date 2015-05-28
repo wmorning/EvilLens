34e2357c7a4b49cc3e916d8271127dae42112e20
Warren Morningstar <wmorning@DN525jhj.SUNet>
2015-05-27 18:01:06 -0700

    changed way saboteur adds phase errors to reduce memory usage.  Also created subfunctions to get alma configuration, so that measurement sets do not have to be used for data (due to problems in drivecasa)

--- evillens/saboteur.py
+++ evillens/saboteur.py
@@ -79,6 +79,13 @@ class Saboteur(object):
         self.antenna2 = np.array((data[0][37].split())[1::3],int)
         
         #get list of antenna coordinates using location given in casapath
+        self.get_antenena_coordinates(antennaconfig)
+        
+# ---------------------------------------------------------------------------
+        
+    def get_antenna_coordinates(self, antennaconfig):
+        casa = drivecasa.Casapy()        
+        
         CASAdir = casa.run_script(['print(" "+os.getenv("CASAPATH").split(" ")[0]+"/data/alma/simmos/")'])[0][0].split()[0]
         antennaparams = np.genfromtxt(str(CASAdir)+str(antennaconfig))
         self.antennaX = antennaparams[:,0]
@@ -112,49 +119,27 @@ class Saboteur(object):
         Add amplitude errors to the visibilities.  Each antenna gets gaussian
         random amplitude error centered around 1 with rms equal to input rms. 
         '''
-        errors = np.random.normal(1.0, rms_error, int(max(self.antenna2)+1.01))
+        errors_real = np.random.normal(1.0, rms_error, int(max(self.antenna2)+1))
+        errors_imag = np.random.normal(1.0,rms_error,int(max(self.antenna2)+1))
     
-        visibilities_new = np.zeros(len(self.Visibilities),complex)
-    
-        for i in range(len(visibilities_new)):
-            visibilities_new[i] = self.Visibilities[i]*errors[self.antenna1[i]]*errors[self.antenna2[i]]
         
-        self.Visibilities=visibilities_new
-
+    
+        for i in range(len(self.Visibilities)):
+            self.Visibilities[i] = self.Visibilities[i].real*errors_real[self.antenna1[i]]*errors_real[self.antenna2[i]] \
+                                    +1j*self.Visibilities[i].imag*errors_imag[self.antenna1[i]]*errors_imag[self.antenna2[i]]
+        
         return
+# ---------------------------------------------------------------------------
         
-# ---------------------------------------------------------------------------    
-    def add_phase_errors(self, v):
+    def get_phases(self,v):
         '''
-        Create coordinate grid of rms phases, using the phase structur function.
-        Assign one phase to each antenna, determined using the antenna's position
-        on the grid.  All visibilities are shifted by the phases assigned to each
-        of their antennas.  This step is performed for several time steps, between
-        which the phase screen is translated.  For simplicity, the
-        translation will always be in the x-direction
-        -v determines the rate at which the phase grid translates.
+        Create coordinate grid of rms phases, using the phase structure function.
         '''
-        
         self.velocity = v   
         Nbaselines = (len(self.antennaX)*(len(self.antennaX)-1))
         Ntsteps = len(self.Visibilities)//Nbaselines
-        
-        #calculate positions of first and second antennas
-        xnew1 = np.zeros(len(self.Visibilities),float)
-        ynew1 = np.zeros(len(self.Visibilities),float)
-        xnew2 = np.zeros(len(self.Visibilities),float)
-        ynew2 = np.zeros(len(self.Visibilities),float)
-        for i in range(len(self.Visibilities)):
-            xnew1[i] = self.antennaX[self.antenna1[i]] +self.velocity*(i//Nbaselines)
-            ynew1[i] = self.antennaY[self.antenna1[i]]
-            xnew2[i] = self.antennaX[self.antenna2[i]] +self.velocity*(i//Nbaselines)
-            ynew2[i] = self.antennaX[self.antenna2[i]]
-        
-        #First determine size of phase screen to within 100m (rounds up)
-        maxX = (np.max([np.max(abs(xnew1)),np.max(abs(xnew2)),np.max(abs(self.antennaY))])\
-                +self.velocity*Ntsteps) //100 *100+100
-
-        
+        maxX = np.max([np.max(abs(self.antennaX))+self.velocity*Ntsteps \
+                       ,np.max(abs(self.antennaY))+self.velocity*Ntsteps])//100 *100 +100
         
         x = np.arange(-maxX,maxX+10.0,10.0)
         y = np.arange(-maxX,maxX+10.0,10.0)
@@ -170,33 +155,43 @@ class Saboteur(object):
                 if np.sqrt(FreqX[i]**2+FreqY[j]**2)>1.0/1000.0:
                     p2[i,j] *= (np.pi/180.0)*(self.K/self.wavelength) \
                                *(0.001/np.sqrt(FreqX[i]**2+FreqY[j]**2))**(5.0/6.0)
-                elif np.sqrt(FreqX[i]**2+FreqY[j]**2)<1.0/1000.0 and np.sqrt(FreqX[i]**2+FreqY[j]**2)>1.0/6000.0:
+                elif np.sqrt(FreqX[i]**2+FreqY[j]**2)<=1.0/1000.0 and np.sqrt(FreqX[i]**2+FreqY[j]**2)>1.0/6000.0:
                     p2[i,j] *= (np.pi/180.0)*(self.K/self.wavelength) \
                                *(0.001/np.sqrt(FreqX[i]**2+FreqY[j]**2))**(1.0/3.0)
                 else:
-                    p2[i,j] *= (np.pi/180.0)*(self.K/self.wavelength)
+                    p2[i,j] *= (np.pi/180.0)*(self.K/self.wavelength)*(6.0)**(1.0/3.0)
                     
         phases = np.fft.ifft2(np.fft.ifftshift(p2,axes={0,1}))
         self.phases=phases.real
+        self.phasecoords_x = x
+        self.phasecoords_y = y
+        self.Nbaselines = Nbaselines
+        self.Ntsteps = Ntsteps
         
-        f_interp = interpolate.RectBivariateSpline(x,y,phases.real,kx=1,ky=1)
         
-        phase_errors1 = np.zeros(len(xnew1),float)
-        phase_errors2 = np.zeros(len(xnew2),float)
-        for i in range(len(phase_errors1)):
-            phase_errors1[i] = f_interp(xnew1[i],ynew1[i])
-            phase_errors2[i] = f_interp(xnew2[i],ynew2[i])
-            
-        self.xnew1 = xnew1
-        self.xnew2 = xnew2
-        self.ynew1 = ynew1
-        self.ynew2 = ynew2
-        self.x = X
-        self.y = Y
-        self.phase_errors1 = phase_errors1
-        self.phase_errors2 = phase_errors2
         
-        self.Visibilities *= np.exp(phase_errors1*1j-phase_errors2*1j)     
+# ---------------------------------------------------------------------------    
+    def add_phase_errors(self, v):
+        '''
+        Create coordinate grid of rms phases, using the phase structure function.
+        Assign one phase to each antenna, determined using the antenna's position
+        on the grid.  All visibilities are shifted by the phases assigned to each
+        of their antennas.  This step is performed for several time steps, between
+        which the phase screen is translated.  For simplicity, the
+        translation will always be in the x-direction
+        -v determines the rate at which the phase grid translates.
+        '''
+        self.get_phases(v)
+        
+        f_interp = interpolate.RectBivariateSpline(self.phasecoords_x,self.phasecoords_y,self.phases,kx=1,ky=1)
+        
+        self.phase_errors1 = np.zeros(len(self.Visibilities),float)
+        self.phase_errors2 = np.zeros(len(self.Visibilities),float)
+        for i in range(len(self.phase_errors1)):
+            self.phase_errors1[i] = f_interp(self.antennaX[self.antenna1[i]]+self.velocity*(i//self.Ntsteps),self.antennaY[self.antenna1[i]])
+            self.phase_errors2[i] = f_interp(self.antennaX[self.antenna2[i]]+self.velocity*(i//self.Ntsteps),self.antennaY[self.antenna2[i]])
+        
+        self.Visibilities *= np.exp(1j*(self.phase_errors1-self.phase_errors2))   
         
         return
         
