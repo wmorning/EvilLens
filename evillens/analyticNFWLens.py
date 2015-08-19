3bd9e06f7eed898253ed62aea0ad88377c6a7e0c
Warren Morningstar <wmorning@DN0a22c161.SUNet>
2015-08-19 14:46:52 -0700

    analytic NFW lens now only has M200 and concentration as input parameters.  Conventions should mesh better with other peoples' needs.

--- evillens/analyticNFWLens.py
+++ evillens/analyticNFWLens.py
@@ -28,29 +28,41 @@ class AnalyticNFWLens(evil.GravitationalLens):
         return
 # ---------------------------------------------------------------------------
 
-    def build_kappa_map(self, r_c, v_c, q=1, centroid = [0,0]):
+    def build_kappa_map(self, M, c, q=1, centroid = [0,0]):
         if q!=1:
             raise Exception('Cant do elliptical NWF lenses yet \n')
-        self.r_c = r_c *units.kpc
-        self.v_c = v_c*units.km/units.s
+            self.e = 1 - q   #conventional ellpiticity = 1 - b / a
+            self.epsilon = (2*(1-self.e)-(1-self.e)**2)/(2-2*(1-self.e)+(1-self.e)**2)
+            self.a1 = 1-self.epsilon  #re-define coordinate system
+            self.a2 = 1+self.epsilon  #using new elliptical coordinates
+            self.x1 = np.sqrt(self.a1)*self.x 
+            self.x2 = np.sqrt(self.a2)*self.y
+            self.xe = np.sqrt(self.x1**2+self.x2**2)
+            self.phi_e = np.arctan(self.x2/self.x1)
+        self.M = M *units.solMass
+        self.c = c
         self.q = q 
         self.centroid = centroid
-        self.theta_c = np.arctan(self.r_c/self.Dd).to(units.arcsec).value
-        self.rho_c = (3.0/8.0*self.v_c**2/self.r_c**2/constants.G).decompose()
+        self.delta_c = 200/3 * c**3/(np.log(1+c)-c/(1+c))
+        self.rho_c = (3.0/8.0 * self.cosmological.H(self.Zd)**2/(8.0*np.pi*constants.G)).to(units.solMass/units.Mpc**3)
+        self.r200 = ((self.M/(800*np.pi/3*self.rho_c))**(1.0/3.0)).to(units.Mpc)
+        self.rs = np.arctan(self.r200 / self.c / self.Dd).to(units.arcsec).value
+        self.kappa_c = 2*(self.rho_c*self.delta_c*self.r200/self.c/self.SigmaCrit).decompose()
         
-        self.kappa_c = (self.rho_c*self.r_c/self.SigmaCrit).decompose()
-        
-        r_norm = np.sqrt((self.x-self.centroid[0])**2+(self.y-self.centroid[1])**2)/self.theta_c
+        r_norm = np.sqrt((self.x-self.centroid[0])**2+(self.y-self.centroid[1])**2)/self.rs
         
         Fx = np.zeros(self.x.shape,float)
-        for i in range(len(self.x[:,0])):
-            for j in range(len(self.x[0,:])):
-                if r_norm[i,j]<1:
-                    Fx[i,j] = 1.0/(r_norm[i,j]**2-1)*(1-np.arccosh(1.0/r_norm[i,j])/np.sqrt(1-r_norm[i,j]**2))
-                elif r_norm[i,j]==1:
-                    Fx[i,j] = 1.0/3.0
-                else:
-                    Fx[i,j] = 1.0/(r_norm[i,j]**2-1)*(1-np.arccos(1/r_norm[i,j])/np.sqrt(r_norm[i,j]**2-1))
+        Fx[np.where(r_norm < 1 )] = 1.0/(r_norm[np.where(r_norm<1)]**2-1.0)*(1.0-(np.arccosh(1.0/r_norm[np.where(r_norm<1)])/np.sqrt(1.0-r_norm[np.where(r_norm<1)]**2)))
+        Fx[np.where(r_norm == 1)] = 1.0/3.0
+        Fx[np.where(r_norm > 1 )] = 1.0/(r_norm[np.where(r_norm>1)]**2-1.0)*(1.0-(np.arccos( 1.0/r_norm[np.where(r_norm>1)])/np.sqrt(r_norm[np.where(r_norm>1)]**2-1.0)))
+#        for i in range(len(self.x[:,0])):
+#            for j in range(len(self.x[0,:])):
+#                if r_norm[i,j]<1:
+#                    Fx[i,j] = 1.0/(r_norm[i,j]**2-1)*(1-np.arccosh(1.0/r_norm[i,j])/np.sqrt(1-r_norm[i,j]**2))
+#                elif r_norm[i,j]==1:
+#                    Fx[i,j] = 1.0/3.0
+#                else:
+#                    Fx[i,j] = 1.0/(r_norm[i,j]**2-1)*(1-np.arccos(1/r_norm[i,j])/np.sqrt(r_norm[i,j]**2-1))
         
         self.kappa = 2.0*self.kappa_c*Fx
         
@@ -58,17 +70,20 @@ class AnalyticNFWLens(evil.GravitationalLens):
         
 # ---------------------------------------------------------------------------        
     def deflect(self):
-        r_norm = np.sqrt((self.image_x-self.centroid[0])**2+(self.image_y-self.centroid[1])**2)/self.theta_c
+        r_norm = np.sqrt((self.image_x-self.centroid[0])**2+(self.image_y-self.centroid[1])**2)/self.rs
         self.r_norm=r_norm
         gx = np.zeros(self.image_x.shape)
-        for i in range(len(self.image_x[:,0])):
-            for j in range(len(self.image_x[0,:])):
-                if r_norm[i,j]<1:
-                    gx[i,j] = np.log(r_norm[i,j]/2.0)+np.arccosh(1.0/r_norm[i,j])/np.sqrt(1-r_norm[i,j]**2)
-                elif r_norm[i,j] ==1:
-                    gx[i,j]=1.0+np.log(0.5)
-                else:
-                    gx[i,j] = np.log(r_norm[i,j]/2.0)+np.arccos(1.0/r_norm[i,j])/np.sqrt(r_norm[i,j]**2-1)
+        gx[np.where(r_norm < 1) ] = np.log(r_norm[np.where(r_norm<1)]/2.0)+np.arccosh(1.0/r_norm[np.where(r_norm<1)])/np.sqrt(1-r_norm[np.where(r_norm<1)]**2)
+        gx[np.where(r_norm == 1)] = 1.0+np.log(0.5)
+        gx[np.where(r_norm > 1) ] = np.log(r_norm[np.where(r_norm>1)]/2.0)+np.arccos(1.0/r_norm[np.where(r_norm>1)])/np.sqrt(r_norm[np.where(r_norm>1)]**2-1)      
+#        for i in range(len(self.image_x[:,0])):
+#            for j in range(len(self.image_x[0,:])):
+#                if r_norm[i,j]<1:
+#                    gx[i,j] = np.log(r_norm[i,j]/2.0)+np.arccosh(1.0/r_norm[i,j])/np.sqrt(1-r_norm[i,j]**2)
+#                elif r_norm[i,j] ==1:
+#                    gx[i,j]=1.0+np.log(0.5)
+#                else:
+#                    gx[i,j] = np.log(r_norm[i,j]/2.0)+np.arccos(1.0/r_norm[i,j])/np.sqrt(r_norm[i,j]**2-1)
         self.alpha_x = (self.image_x-self.centroid[0])*(4.0*self.kappa_c*gx)/r_norm**2
         self.alpha_y = (self.image_y-self.centroid[1])*(4.0*self.kappa_c*gx)/r_norm**2
         
