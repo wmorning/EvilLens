f03d83763ad75ab40655f939d8471d1b8fcaace8
Warren Morningstar <wmorning@DN525j9f.SUNet>
2015-12-01 17:29:22 -0800

    new build_from_clumps function.  Looks less fake.

--- evillens/source.py
+++ evillens/source.py
@@ -8,6 +8,10 @@ between their pixel size and physical scale in arcsec.
 
 import numpy as np
 from astropy.io import fits 
+from astropy import units,constants
+from astropy.cosmology import FlatLambdaCDM
+import scipy.special as sp
+import math
 
 # ======================================================================
 
@@ -16,11 +20,15 @@ class Source(object):
     def __init__(self, Zs):  
     
         self.Zs = Zs
+        self.compute_distances()
     
         return    
 # ----------------------------------------------------------------------
-    
-    def Read_Source_from(self, fitsfile):
+    def compute_distances(self):
+        self.cosmological = FlatLambdaCDM(H0=71.0, Om0=0.2669)
+        self.Ds = self.cosmological.angular_diameter_distance(self.Zs)
+# ----------------------------------------------------------------------   
+    def read_source_from(self, fitsfile):
         '''
         Read an image from a fitsfile, and setup its grid.  Here we need
         to properly read in the wcs coordinate information so that the 
@@ -99,7 +107,88 @@ class Source(object):
         return        
 
 # ----------------------------------------------------------------------
- 
+    def build_from_clumps(self,size=2.0,clump_size = 0.1,clump_size_rms=0.0005,axis_ratio=1.0, orientation=0.0,center=[0,0], Nclumps=50, n = 1 , error =10**-8):
+        #raise Exception("cannot build source from clumps yet. \n")
+        '''
+        Build source from gaussian clumps centered about specified position.
+        Accepted parameters are as follows:
+        - Size of the source, in kpc (approximately the half light radius)
+        - Size of individual clumps.  RMS determines spread in clump size.
+        - Axis ratio, or the ratio of the major axis to the minor axis.
+        - Orientation angle of the source measured in radians from the 
+           positive x-axis.
+        - Position of the source center (in arcsec).
+        - The number of clumps making up the source.
+        - The average brightness of each clump.
+        - The standard deviation of the brightness for individual clumps.
+           The clump brightness follows a lognormal distribution
+        - Sersic index n
+        '''
         
+        self.axis_ratio = axis_ratio
+        self.orientation = orientation
+        self.Nclumps = Nclumps
+        self.center = center
+        self.n = n
+        #compute rms radius in arcsec
+        self.size = np.arctan((size *units.kpc) / self.Ds ).to(units.arcsec).value
+        self.clump_size = np.arctan((clump_size*units.kpc)/self.Ds).to(units.arcsec).value
+        self.clump_size_rms = np.arctan((clump_size_rms*units.kpc)/self.Ds).to(units.arcsec).value     
+        #pick random positions inside of 4r_eff          
+#        rlist = np.sqrt(np.random.random(Nclumps))*4.0*self.size
+#        thetalist = np.random.random(Nclumps)*2*np.pi
+        xpos_orig = np.random.exponential(self.size,Nclumps)/np.sqrt(self.axis_ratio)*np.random.choice([-1,1],Nclumps)
+        ypos_orig = np.random.exponential(self.size,Nclumps)*np.sqrt(self.axis_ratio)*np.random.choice([-1,1],Nclumps)
+        self.xlist = xpos_orig*np.cos(self.orientation)-ypos_orig*np.sin(self.orientation) + self.center[0]
+        self.ylist = xpos_orig*np.sin(self.orientation)+ypos_orig*np.cos(self.orientation) +self.center[1]
+        
+        #determine constant b_n which allows us to use r as half light radius
+        def fx(n,bn):
+            return(2*sp.gammainc(2*n,bn)-1)
+        
+        #initial guess follows approximation from Capaccioli 1989
+        x0 = 1.9992*n - 0.3271
+        x1 = x0 + 0.01
+        j = 0
+        epsilon = abs(x1-x0)
+        while epsilon > error and j<100000:
+            fx0 = fx(n,x0)
+            fx1 = fx(n,x1)
+            x0,x1 = x1 , x1 - fx1*(x1-x0) / (fx1-fx0)
+            epsilon = abs(x1-x0)
+            j+=1
+        if j ==100000:
+            #solution didn't converge, lets just approximate it.
+            self.b_n = 1.9992*n-0.3271
+        else:
+            self.b_n = x1
+        
+        #self.Blist = np.exp(-self.b_n*((np.sqrt((np.cos(self.orientation)*(self.xlist-self.center[0])-np.sin(self.orientation)*(self.ylist-self.center[1]))**2*self.axis_ratio+((self.xlist-self.center[0])*np.sin(self.orientation)+(self.ylist-self.center[1])*np.cos(self.orientation))**2/self.axis_ratio)/self.size)**(1/self.n)-1))        
+        self.Slist = abs(np.random.normal(self.clump_size,self.clump_size_rms,self.Nclumps))
+        
+        for i in range(self.Nclumps):
+            if i==0:
+                self.intensity = np.exp(-0.5*((self.beta_x-self.xlist[i])**2+(self.beta_y-self.ylist[i])**2)/self.Slist[i]**2)
+            else:
+                self.intensity +=np.exp(-0.5*((self.beta_x-self.xlist[i])**2+(self.beta_y-self.ylist[i])**2)/self.Slist[i]**2)
+        
+
+        self.intensity *= np.exp(-self.b_n*((np.sqrt((np.cos(self.orientation)*(self.beta_x-self.center[0])+np.sin(self.orientation)*(self.beta_y-self.center[1]))**2*self.axis_ratio+(-(self.beta_x-self.center[0])*np.sin(self.orientation)+(self.beta_y-self.center[1])*np.cos(self.orientation))**2/self.axis_ratio)/self.size)**(1/self.n)-1))        
+
+        return
+        
+# ----------------------------------------------------------------------
+
+    def write_source_to(self,fitsfile,overwrite=False):
+        '''
+        Write an image of the source to a fits file.
+        '''
+        
+        hdu = fits.PrimaryHDU(self.intensity)
+        hdu.header['CDELT1'] = self.pixscale / 3600.0
+        hdu.header['CDELT2'] = self.pixscale / 3600.0
+        hdu.writeto(fitsfile, clobber=overwrite)
+        
+        return
         
 # ======================================================================
